1a8ba404f338edb373667bb929aaab3638befa44
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-05 17:30:45 +0200

    esercizio9

---
+++ Esercizio6/src/index.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App'; // Adjust the path if necessary
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
