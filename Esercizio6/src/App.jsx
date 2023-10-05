ff345bec544054ec77f8581381273dc8aeafb463
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-05 18:23:42 +0200

    esercizio13

--- Esercizio6/src/App.jsx
+++ Esercizio6/src/App.jsx
@@ -7,11 +7,14 @@ function App() {
   return (
     <div>
     <Helloworld/>
+    <Helloworld/>
     <Message/>
     </div>
   )
 }
 
 export default App
+
+/* Posso utilizzare due volte lo stesso componente e verrà visualizzato due volte a schermo */
 
 
