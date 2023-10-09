545fed0d3e846fdf565a2d96b1aea792586d784d
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-09 11:39:03 +0200

    esercizio17

--- Esercizio6/src/App.jsx
+++ Esercizio6/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
     <>
     <Helloworld/>
     <Message/>
-    <Welcome name="Vicio"/>
+    <Welcome/>
     </>
   )
 }
--- Esercizio6/src/Welcome.jsx
+++ Esercizio6/src/Welcome.jsx
@@ -1,3 +1,6 @@
 export function Welcome({name}) {
   return <p> Welcome, {name}! </p>;
 }
+
+
+/* succede che il nome non viene visualizzato, posso settare un nome di default nel caso in cui manchi all'elemento genitore*/
