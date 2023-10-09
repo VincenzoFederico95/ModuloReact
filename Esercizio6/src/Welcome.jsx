8f48234ec625b7591c3ecad97d78c49ee0da5d72
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-09 12:20:54 +0200

    esercizio24

--- Esercizio6/src/Welcome.jsx
+++ Esercizio6/src/Welcome.jsx
@@ -4,8 +4,8 @@ import { Age } from "./Age";
 export function Welcome({name, age}) {
   return (
     <>
-  <p> Welcome, {name}! </p>
-  { 18 < age < 65 ? <Age age={age} /> : "too young or too old for this application"}
+   <p>Welcome, {name === "John" ? name : " "}!  </p>
+      { 18 < age < 65 ? <Age age={age} /> : "too young or too old for this application"}
   </>
   );
 }
