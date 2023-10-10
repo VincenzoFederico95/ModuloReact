f4310b01a2fe6e9a1d5b27273211778bd8520f97
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-10 11:45:10 +0200

    esercizio38

--- Esercizio6/src/Mouseclicker.jsx
+++ Esercizio6/src/Mouseclicker.jsx
@@ -4,7 +4,9 @@ export function MouseClicker({ name }) {
     }
     return (
       <button name={name} onClick={handleClick}>
-        <img name = "image"  width={50} height={50} />
+        <img onClick={handleImg} name = "image" src="https://www.perlesandco.it/images/product/1/3/1/131731_moyen/im-Bottone-rotondo-2-fori-cucito-camicia-11-mm-nero-x1-.jpg"  width={50} height={50} />
       </button>
     );
   }
+
+ /* si, il nome viene comunque stampato in console graie all'evento collegato al pulsante, tuttavia verrà attivato dall'immagine */
