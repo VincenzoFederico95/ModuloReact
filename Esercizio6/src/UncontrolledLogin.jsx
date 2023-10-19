6bc729663bda2e9dbe5606a9cf04a0d742b7e9cd
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-19 15:52:02 +0200

    47

--- Esercizio6/src/UncontrolledLogin.jsx
+++ Esercizio6/src/UncontrolledLogin.jsx
@@ -1,11 +1,23 @@
 export function UncontrolledLogin() {
-    return (
-      <form action="#">
-        <input type="text" name="username" />
-        <input type="password" name="password" />
-        <input type="checkbox" name="remember" />
-        <button>Login</button>
-        <button>Reset</button>
-      </form>
-    )
-  }
+    function handleFormSubmit(event) {
+        event.preventDefault();
+        const username = event.target.elements.namedItem('username').value;    
+        const password = event.target.elements.namedItem('password').value;
+        const data = {
+            username,
+            password
+        }
+        console.log(data);
+    }
+
+
+  return (
+    <form action="#" onSubmit={handleFormSubmit}>
+      <input type="text" name="username" />
+      <input type="password" name="password" />
+      <input type="checkbox" name="remember" />
+      <button>Login</button>
+      <button>Reset</button>
+    </form>
+  )
+}
