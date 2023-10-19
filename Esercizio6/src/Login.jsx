d9421edd73cd07b90401f42fa2d87f79e40ceb84
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-19 15:38:14 +0200

    43

--- Esercizio6/src/Login.jsx
+++ Esercizio6/src/Login.jsx
@@ -1,29 +1,48 @@
 import { useState } from "react";
 
-export function Login() {
-    function useData() {
-        return { password: "", username: "", remember: false}
+export function Login({onLogin}) {
+  function useData() {
+    return { password: "", username: "", remember: false };
   }
-    const [data, setData] = useState(useData())
+  const [data, setData] = useState(useData());
 
-    function handleDataChange(e) {
-        const name = e.target.name;
-        const value = e.target.value;
-        const checked = e.target.checked;
-        const type = e.target.type;
-        setData((data) => {
-            return {...data,
-            [name] : type === 'checkbox' ? checked : value}
-        })
+  function handleDataChange(e) {
+    const name = e.target.name;
+    const value = e.target.value;
+    const checked = e.target.checked;
+    const type = e.target.type;
 
-        
-}
-    
-return (<div>
-    <input type="text" name="username" value = {data.username} onChange={handleDataChange} />
-    <input type="password" name="password" value = {data.password} onChange={handleDataChange}/>
-    <input type="checkbox" name="remember" checked = {data.remember} onChange={handleDataChange} />
-  
-</div>) 
+    setData((data) => {
+      return { ...data, [name]: type === "checkbox" ? checked : value };
+    });
+  }
 
+  function handleLogin() {
+    onLogin(data);
+  }
+  return (
+    <div>
+      <input
+        type="text"
+        name="username"
+        value={data.username}
+        onChange={handleDataChange}
+      />
+      <input
+        type="password"
+        name="password"
+        value={data.password}
+        onChange={handleDataChange}
+      />
+      <input
+        type="checkbox"
+        name="remember"
+        checked={data.remember}
+        onChange={handleDataChange}
+      />
+      <button disabled={!data.username || !data.password} onClick={handleLogin}>
+        Login
+      </button>
+    </div>
+  );
 }
