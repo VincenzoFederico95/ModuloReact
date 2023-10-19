3b382d94aae6aaa9049c5153cbece05cd9a7913a
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-19 16:17:34 +0200

    52

--- Esercizio6/src/Counter.jsx
+++ Esercizio6/src/Counter.jsx
@@ -1,14 +1,24 @@
 import { useEffect } from "react";
 import { CounterDisplay } from "./CounterDisplay";
+import { useRef } from "react";
 
-export function Counter({ counter, incrementCounter, decrementCounter, reset}) {
-  useEffect(()=>{ console.log(counter)}, [counter])
-    return (
-      <div>
-        <CounterDisplay count={counter} />
-        <button onClick={incrementCounter}>Increment the number </button>
-        <button onClick={decrementCounter}>Decrement the number </button>
-        <button onClick={reset}> Reset </button>
-      </div>
-    );
-  }
+export function Counter({
+  counter,
+  incrementCounter,
+  decrementCounter,
+  reset,
+}) {
+  const counterRef = useRef();
+  useEffect(() => {
+    console.log(counterRef.current.textContent);
+  }, [counter]);
+
+  return (
+    <div>
+      <CounterDisplay counterRef={counterRef} count={counter} />
+      <button onClick={incrementCounter}>Increment the number </button>
+      <button onClick={decrementCounter}>Decrement the number </button>
+      <button onClick={reset}> Reset </button>
+    </div>
+  );
+}
