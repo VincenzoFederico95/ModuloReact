80c64c6e64f64546bbc1df575cce2b5e66416868
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-21 14:33:55 +0200

    61

--- Esercizio6/src/Counterdisplay.jsx
+++ Esercizio6/src/Counterdisplay.jsx
@@ -1,3 +1,16 @@
-export function CounterDisplay({count}) {
-    return <h2>{count} </h2>   
-   }
+export function CounterDisplay({ count, counterRef }) {
+    const CounterStyle = {
+      padding: "20px",
+      backgroundColor: "bisque",
+      color: "hotPink",
+      display: "flex",
+      width: "50px",
+      justifyContent: "center",
+    };
+  
+    return (
+      <h2 style={CounterStyle} ref={counterRef}>
+        {count}
+      </h2>
+    );
+  }
