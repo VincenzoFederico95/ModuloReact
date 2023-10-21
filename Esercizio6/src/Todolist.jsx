71bf69ae7f29596d7262ddf6b0a56f30c08a8d21
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-21 13:25:53 +0200

    56

---
+++ Esercizio6/src/Todolist.jsx
@@ -0,0 +1,20 @@
+import { useRef, useState } from "react";
+
+export function ToDoList() {
+  const [items, setItems] = useState([]);
+  const inputRef = useRef(null);
+  function handleButton() {
+    setItems([...items, inputRef.current.value]);
+  }
+  return (
+    <>
+      <ul>
+        {items.map((item, index) => (
+          <li key={index}>{item}</li>
+        ))}
+      </ul>
+      <input ref={inputRef} type="text" />
+      <button onClick={handleButton}> Add some </button>
+    </>
+  );
+}
