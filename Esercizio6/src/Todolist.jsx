06b33da9896f7b7cabf0e462858048c97b328a10
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-21 13:31:58 +0200

    59

--- Esercizio6/src/Todolist.jsx
+++ Esercizio6/src/Todolist.jsx
@@ -1,25 +1,36 @@
-mport { useRef, useState } from "react";
+import { useRef, useState } from "react";
 
 export function ToDoList() {
   const [items, setItems] = useState([]);
-  const inputRef = useRef(null);
+  const inputRef = useRef();
+
   function handleButton() {
     setItems([...items, inputRef.current.value]);
-    inputRef.current.value = " ";
+    inputRef.current.value = "";
   }
-  
+
   function handleResetButton() {
-    setItems([])
+    setItems([]);
   }
+
+  function handleRemoveButton(index) {
+    const updatedItems = [...items];
+    updatedItems.splice(index, 1);
+    setItems(updatedItems);
+  }
+
   return (
     <>
       <ul>
         {items.map((item, index) => (
-          <li key={index}>{item}</li>
+          <li key={index}>
+            {item}
+            <button onClick={() => handleRemoveButton(index)}>Remove</button>
+          </li>
         ))}
       </ul>
       <input ref={inputRef} type="text" />
-      <button onClick={handleButton}> To do </button>
+      <button onClick={handleButton}>To do</button>
       <button onClick={handleResetButton}>Reset</button>
     </>
   );
