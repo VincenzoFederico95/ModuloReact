bdd47b9ac00d48b6c29c41cfaecdb36917c01d1f
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-21 13:25:20 +0200

    56

--- Esercizio6/src/App.jsx
+++ Esercizio6/src/App.jsx
@@ -1,72 +1,79 @@
 import { AlertClock } from "./Alertclock";
 import { Clock } from "./Clock";
 import { Counter } from "./Counter";
-import { Helloworld } from "./Helloworld"
+import { Helloworld } from "./Helloworld";
 import { InteractiveWelcome } from "./InteractiveWelcome";
 import { Login } from "./Login";
-import { Message } from "./Message"
+import { Message } from "./Message";
 import { MouseClicker } from "./Mouseclicker";
 import { MultiButton } from "./Multibutton";
-import { Welcome } from "./Welcome"
+import { Welcome } from "./Welcome";
 import { useState } from "react";
 import { Focusableinputs } from "./Focusableinput";
 import { Colors } from "./Color";
-
+import { ToDoList } from "./Todolist";
 
 function App() {
   function handleButtonClick() {
     const time = new Date();
     alert(`The current time is: ${time.toLocaleTimeString()}`);
   }
-  const [counter, setCounter] = useState(0)
-    function handleCounter() {
-        setCounter (counter +1 )
-    }
-    function decrementCounter() {
-      setCounter (counter - 1 )
+  const [counter, setCounter] = useState(0);
+  function handleCounter() {
+    setCounter(counter + 1);
+  }
+  function decrementCounter() {
+    setCounter(counter - 1);
   }
   function reset() {
-      setCounter (0)
+    setCounter(0);
+  }
+
+  function handleLogin(data) {
+    console.log("The user data are: ", data);
   }
 
   return (
     <>
-      <Helloworld/>
+      <Helloworld />
       <Message />
       <Welcome name={<strong>John</strong>} age={44} />
       <AlertClock buttonClicked={handleButtonClick} />
-      <Counter counter={counter} incrementCounter={incrementCounter} decrementCounter={decrementCounter}  reset = {reset}/>
-      <Clock/>
-      <MouseClicker name={"one"}/>
-      <MultiButton nameOne="one" nameTwo ="two"  nameThree = "three"/>
-      <InteractiveWelcome/>
-      <Login/>
-      <Focusableinputs/>
+      <Counter
+        counter={counter}
+        incrementCounter={incrementCounter}
+        decrementCounter={decrementCounter}
+        reset={reset}
+      />
+      <Clock />
+      <MouseClicker name={"one"} />
+      <MultiButton nameOne="one" nameTwo="two" nameThree="three" />
+      <InteractiveWelcome />
+      <Login />
+      <Focusableinputs />
       <Colors
-      items={[{
-        id:1,
-        name: "VicIo",
-        },
+        items={[
           {
-        id:2,
-        name: "Fede",
-        },
+            id: 1,
+            name: "VicIo",
+          },
           {
-        id:3,
-        name: "Giulio",
-        },
+            id: 2,
+            name: "Fede",
+          },
           {
-        id:4,
-        name: " Luca",
-      }]
-      }/>
-     </>
-
+            id: 3,
+            name: "Giulio",
+          },
+          {
+            id: 4,
+            name: " Luca",
+          },
+        ]}
+      />
+      <ToDoList/>
+    </>
   );
 }
 
 export default App;
-
-
-
-
