4cbe7e36c71795164607309413b90e6de431d450
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-25 17:47:18 +0200

    38

--- Esercizio6/src/Mouseclicker.jsx
+++ Esercizio6/src/Mouseclicker.jsx
@@ -1,12 +1,16 @@
 export function MouseClicker({ name }) {
-    function handleClick(e) {
-      console.log(e.target.name);
+  function handleClick(e) {
+    console.log(e.target.name);
     }
-    return (
-      <button name={name} onClick={handleClick}>
-        <img onClick={handleImg} name = "image" src="https://www.perlesandco.it/images/product/1/3/1/131731_moyen/im-Bottone-rotondo-2-fori-cucito-camicia-11-mm-nero-x1-.jpg"  width={50} height={50} />
-      </button>
-    );
-  }
+    
+    function handleImg(e) {
+        console.log(e.target.src)
+    }
+  return (
+    <button name={name} onClick={handleClick}>
+      <img onClick={handleImg} name = "image" src="https://www.educolor.it/immagine-bottone-dl28074.jpg" width={50} height={50} />
+    </button>
+  );
+}
 
- /* si, il nome viene comunque stampato in console graie all'evento collegato al pulsante, tuttavia verrà attivato dall'immagine */
+/* si, il nome viene comunque stampato in console graie all'evento collegato al pulsante, tuttavia verrà attivato dall'immagine */
