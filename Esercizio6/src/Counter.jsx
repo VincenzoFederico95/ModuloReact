1b65ccd403ee4944b75e35d0c86263769c7064b6
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-25 13:08:18 +0200

    33

--- Esercizio6/src/Counter.jsx
+++ Esercizio6/src/Counter.jsx
@@ -1,12 +1,12 @@
 import { CounterDisplay } from "./CounterDisplay";
 
 export function Counter({ counter, incrementCounter, decrementCounter, reset}) {
-    return (
-      <div>
-        <CounterDisplay count={counter} />
-        <button onClick={incrementCounter}>Increment the number </button>
-        <button onClick={decrementCounter}>Decrement the number </button>
-        <button onClick={reset}> Reset </button>
-      </div>
-    );
-  }
+  return (
+    <div>
+      <CounterDisplay count={counter} />
+      <button onClick={incrementCounter}>Increment the number </button>
+      <button onClick={decrementCounter}>Decrement the number </button>
+      <button onClick={reset}> Reset </button>
+    </div>
+  );
+}
