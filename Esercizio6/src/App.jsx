215e0af2a24f2bc20300b53d24f0fc96a62afa44
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-25 17:52:20 +0200

    39

--- Esercizio6/src/App.jsx
+++ Esercizio6/src/App.jsx
@@ -1,45 +1,39 @@
-import { AlertClock } from "./Alertclock";
-import { Clock } from "./Clock";
+import { Helloworld } from "./Helloworld";
+import { Message } from "./Message";
+import { Welcome } from "./Welcome";
+import { AlertClock } from "./AlertClock";
 import { Counter } from "./Counter";
-import { Helloworld } from "./Helloworld"
-import { Message } from "./Message"
-import { MouseClicker } from "./Mouseclicker";
-import { Welcome } from "./Welcome"
 import { useState } from "react";
-
+import { Clock } from "./Clock";
+import { MouseClicker } from "./MouseClicker";
 
 function App() {
   function handleButtonClick() {
     const time = new Date();
     alert(`The current time is: ${time.toLocaleTimeString()}`);
   }
-  const [counter, setCounter] = useState(0)
-    function handleCounter() {
+   const [counter, setCounter] = useState(0)
+    function incrementCounter () {
         setCounter (counter +1 )
     }
     function decrementCounter() {
-      setCounter (counter - 1 )
-  }
-  function reset() {
-      setCounter (0)
-  }
+        setCounter (counter - 1 )
+    }
+    function reset() {
+        setCounter (0)
+    }
 
   return (
-    <>
-      <Hello />
+    <div>
+      <Helloworld />
       <Message />
       <Welcome name={<strong>John</strong>} age={44} />
       <AlertClock buttonClicked={handleButtonClick} />
-      <Counter counter={counter} incrementCounter={incrementCounter} decrementCounter={decrementCounter}  reset = {reset}/>
-      <Clock/>
-      <MouseClicker name={"one"}/>
-     </>
-
+      <Counter counter={counter} incrementCounter={incrementCounter} decrementCounter={decrementCounter}  reset = {reset} />
+      <Clock />
+      <MouseClicker name = "one" />
+    </div>
   );
 }
 
 export default App;
-
-
-
-
--- Esercizio6/src/Mouseclicker.jsx
+++ Esercizio6/src/Mouseclicker.jsx
@@ -1,15 +1,21 @@
 export function MouseClicker({ name }) {
-    function handleClick(e) {
-        console.log(e.currentTarget.name);
-    }
-    function handleImg(e) {
-        console.log(e.target.src)
-    }
-    return (
-      <button name={name} onClick={handleClick}>
-        <img onClick={handleImg} name = "image" src="https://www.perlesandco.it/images/product/1/3/1/131731_moyen/im-Bottone-rotondo-2-fori-cucito-camicia-11-mm-nero-x1-.jpg"  width={50} height={50} />
-      </button>
-    );
+  function handleClick(e) {
+    console.log(e.currentTarget.name);
   }
+  function handleImg(e) {
+    console.log(e.target.src);
+  }
+  return (
+    <button name={name} onClick={handleClick}>
+      <img
+        onClick={handleImg}
+        name="image"
+        src="https://www.perlesandco.it/images/product/1/3/1/131731_moyen/im-Bottone-rotondo-2-fori-cucito-camicia-11-mm-nero-x1-.jpg"
+        width={50}
+        height={50}
+      />
+    </button>
+  );
+}
 
- /* Per farlo, si può utilizzare la proprietà dell'evento CurrentTarget, che consente di individuare l'elemento a cui è collegato il gestore di eventi */
+/* Per farlo, si può utilizzare la proprietà dell'evento CurrentTarget, che consente di individuare l'elemento a cui è collegato il gestore di eventi */
