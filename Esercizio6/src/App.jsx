3790f2f61a46bebea1ae27f34048206d12900b07
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-25 17:54:28 +0200

    40

--- Esercizio6/src/App.jsx
+++ Esercizio6/src/App.jsx
@@ -1,47 +1,41 @@
-import { AlertClock } from "./Alertclock";
-import { Clock } from "./Clock";
+import { Helloworld } from "./Helloworld";
+import { Message } from "./Message";
+import { Welcome } from "./Welcome";
+import { AlertClock } from "./AlertClock";
 import { Counter } from "./Counter";
-import { Helloworld } from "./Helloworld"
-import { Message } from "./Message"
-import { MouseClicker } from "./Mouseclicker";
-import { MultiButton } from "./Multibutton";
-import { Welcome } from "./Welcome"
 import { useState } from "react";
-
+import { Clock } from "./Clock";
+import { MouseClicker } from "./MouseClicker";
+import { MultiButton } from "./MultiButton";
 
 function App() {
   function handleButtonClick() {
     const time = new Date();
     alert(`The current time is: ${time.toLocaleTimeString()}`);
   }
-  const [counter, setCounter] = useState(0)
-    function handleCounter() {
+   const [counter, setCounter] = useState(0)
+    function incrementCounter () {
         setCounter (counter +1 )
     }
     function decrementCounter() {
-      setCounter (counter - 1 )
-  }
-  function reset() {
-      setCounter (0)
-  }
+        setCounter (counter - 1 )
+    }
+    function reset() {
+        setCounter (0)
+    }
 
   return (
-    <>
-      <Hello />
+    <div>
+      <Helloworld />
       <Message />
       <Welcome name={<strong>John</strong>} age={44} />
       <AlertClock buttonClicked={handleButtonClick} />
-      <Counter counter={counter} incrementCounter={incrementCounter} decrementCounter={decrementCounter}  reset = {reset}/>
-      <Clock/>
-      <MouseClicker name={"one"}/>
+      <Counter counter={counter} incrementCounter={incrementCounter} decrementCounter={decrementCounter}  reset = {reset} />
+      <Clock />
+      <MouseClicker name="one" />
       <MultiButton nameOne="one" nameTwo ="two"  nameThree = "three"/>
-     </>
-
+    </div>
   );
 }
 
 export default App;
-
-
-
-
