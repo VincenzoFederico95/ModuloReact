92596276dc317a6f215549ebac9b100dc77b3dce
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-10-25 12:20:55 +0200

    22

--- Esercizio6/src/Welcome.jsx
+++ Esercizio6/src/Welcome.jsx
@@ -1,13 +1,10 @@
-
 import { Age } from "./Age";
 
-export function Welcome({name, age}) {
+export function Welcome({ name, age }) {
   return (
     <>
-  <p> Welcome, {name}! </p>
-  {age ? <Age age={age} /> : "sorry, age not declared"}
-  </>
+      <p> Welcome, {name}! </p>
+      {age ? <Age age={age} /> : "sorry, age not declared"}
+    </>
   );
 }
-
-
