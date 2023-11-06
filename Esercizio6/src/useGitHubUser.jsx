218cc22790671c7f7bd4dd69c3195d240bbcc6ad
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-11-06 12:58:59 +0100

    90

--- Esercizio6/src/useGitHubUser.jsx
+++ Esercizio6/src/useGitHubUser.jsx
@@ -3,6 +3,15 @@ import useSWR from 'swr'
 const fetcher = url => fetch(url).then(response => response.json())
 
 const useGitHubUser = (username) => {
+if (username === null) {
+    return {
+      data: null,
+      error: null,
+      loading: false,
+    };
+  }
+
+
 const{data, error} = useSWR(`https://api.github.com/users/${username}`, fetcher);
   return {
     data,
