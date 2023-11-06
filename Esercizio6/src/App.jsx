e57f81da736d6a0cfd08b4cb9b9201cd3293e181
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-11-06 12:39:48 +0100

    85

--- Esercizio6/src/App.jsx
+++ Esercizio6/src/App.jsx
@@ -1,4 +1,5 @@
-import { Route, Routes } from "react-router-dom";
+
+import { Link, Route, Routes } from "react-router-dom";
 import { Welcome } from "./Welcome";
 import { Counter } from "./Counter";
 import ShowGithubUser from "./ShowGithubUser";
@@ -6,15 +7,19 @@ import ShowGithubUser from "./ShowGithubUser";
 const App = () => {
   return (
     <>
+      <nav>
+        <Link to="/"> Home </Link>
+        <Link to="/counter"> Counter </Link>
+        <Link to="/users/:username"> Users </Link>
+      </nav>
+
       <Routes>
         <Route path="/" element={<Welcome />} />
         <Route path="/counter" element={<Counter />} />
-        <Route path="/users/:username" element={<ShowGithubUser/>} />
+        <Route path="/users/:username" element={<ShowGithubUser />} />
         <Route path="/:name" element={<Welcome />} />
       </Routes>
-
     </>
-
   );
 };
 
