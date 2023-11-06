00a5354755f2e1a42d79b2fcc6550f8bd50f97d0
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-11-06 12:31:12 +0100

    82

--- Esercizio6/src/App.jsx
+++ Esercizio6/src/App.jsx
@@ -1,103 +1,21 @@
-import { AlertClock } from "./Alertclock";
-import { Clock } from "./Clock";
-import { Counter } from "./Counter";
-import { Helloworld } from "./Helloworld";
-import { InteractiveWelcome } from "./InteractiveWelcome";
-import { Login } from "./Login";
-import { Message } from "./Message";
-import { MouseClicker } from "./Mouseclicker";
-import { MultiButton } from "./Multibutton";
-import { Welcome } from "./Welcome";
-import { useState } from "react";
-import { Focusableinputs } from "./Focusableinput";
-import { Colors } from "./Color";
-import { ToDoList } from "./Todolist";
-import { Container } from "./Container";
-import GithubUsersList from "./GithubUsersList";
-import CurrentLocation from "./CurrentLocation";
+/*Create an App component that wraps a Routes
+ component and add a single Route to the / path that
+ renders the Welcome component from the 
+ first exercise of Styling Component, passing it a name prop.
+  Render the App component within a BrowserRouter component. */
 
-function App() {
-  function handleButtonClick() {
-    const time = new Date();
-    alert(`The current time is: ${time.toLocaleTimeString()}`);
-  }
-  const [counter, setCounter] = useState(0);
-  function handleCounter() {
-    setCounter(counter + 1);
-  }
-  function decrementCounter() {
-    setCounter(counter - 1);
-  }
-  function reset() {
-    setCounter(0);
-  }
-
-  function handleLogin(data) {
-    console.log("The user data are: ", data);
-  }
-
-  const [language, setLanguage] = useState("en");
-
-  function handleLanguageSelection(language) {
-    setLanguage(language);
-  }
-
-
-  return (
-    <>
-      <Container title={<h1> This is my awesome container </h1>}></Container>
-      <Helloworld />
-      <Message />
-      <Welcome name={<strong>John</strong>} age={44} />
-      <AlertClock buttonClicked={handleButtonClick} />
-      <Counter
-        counter={counter}
-        incrementCounter={incrementCounter}
-        decrementCounter={decrementCounter}
-        reset={reset}
-      />
-      <select
-        onChange={() => handleLanguageSelection("IT")}
-        name="language"
-        id="language"
-      >
-        <option value="en">EN</option>
-        <option value="it">IT</option>
-      </select>
-
-      <Clock />
-
-      <MouseClicker name={"one"} />
-      <MultiButton nameOne="one" nameTwo="two" nameThree="three" />
-      <InteractiveWelcome />
-      <Login onLogin={handleLogin} />
-      <Focusableinputs />
-      <Colors
-        items={[
-          {
-            id: 1,
-            name: "VicIo",
-          },
-          {
-            id: 2,
-            name: "Fede",
-          },
-          {
-            id: 3,
-            name: "Giulio",
-          },
-          {
-            id: 4,
-            name: " Luca",
-          },
-        ]}
-      />
-      <ToDoList />
-      <Container />
-      <GithubUsersList />
-      
-    </>
-  );
-}
-
-export default App;
+  import { Route, Routes } from "react-router-dom";
+  import { Welcome } from "./Welcome";
+  
+  const App = () => {
+    return (
+      <>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/:name" element={<Welcome />} />
+        </Routes>
+      </>
+    );
+  };
+  
+  export default App;
---
+++ Esercizio6/src/Root,jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter } from 'react-router-dom'
+import App from './App'
+import React from 'react'
+
+const Root = () => {
+  return (
+      <>
+          <BrowserRouter>
+          <App/>
+          </BrowserRouter>
+
+    </>
+  )
+}
+
+export default Root;
