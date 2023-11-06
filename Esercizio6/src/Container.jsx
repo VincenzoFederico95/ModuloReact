b08218a0dd44c5a204e16fa0ad6b5a4e7fc4dbe4
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-11-06 11:04:54 +0100

    67

---
+++ Esercizio6/src/Container.jsx
@@ -0,0 +1,8 @@
+export function Container({ children }) {
+    return (
+      <div style={{ border: "1px solid blue", backgroundColor: "beige" }}>
+        {children}
+      </div>
+    );
+  }
+  //non ho scaricato Tailwind
