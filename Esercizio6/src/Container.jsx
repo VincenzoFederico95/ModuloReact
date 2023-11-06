b370e1782548b5fb3c21173cf9150ba2b4e597f1
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-11-06 11:10:29 +0100

    69

--- Esercizio6/src/Container.jsx
+++ Esercizio6/src/Container.jsx
@@ -1,10 +1,20 @@
+import { useState } from "react";
+
 export function Container({ children, title }) {
+  const [collapsed, setCollapsed] = useState(null);
+  function handleCollapseButton() {
+    setCollapsed((c) => !c);
+  }
+   
   return (
     <>
       {title}
-      <div style={{ border: "1px solid blue", backgroundColor: "beige" }}>
-        {children}
-      </div>
+      {collapsed && (
+        <div style={{ border: "1px solid blue", backgroundColor: "beige" }}> 
+          {children}
+        </div>
+      )}
+      <button onClick={handleCollapseButton}> Show more </button>
     </>
   );
 }
