22a40db80500f3ad489dc5ce78c97db4e3b25e1e
VincenzoFederico95 <vincenzofederico95@outlook.it>
2023-11-06 12:33:54 +0100

    83

--- Esercizio6/src/App.jsx
+++ Esercizio6/src/App.jsx
@@ -1,21 +1,16 @@
-/*Create an App component that wraps a Routes
- component and add a single Route to the / path that
- renders the Welcome component from the 
- first exercise of Styling Component, passing it a name prop.
-  Render the App component within a BrowserRouter component. */
 
-  import { Route, Routes } from "react-router-dom";
-  import { Welcome } from "./Welcome";
-  
-  const App = () => {
-    return (
-      <>
-        <Routes>
-          <Route path="/" element={<Welcome />} />
-          <Route path="/:name" element={<Welcome />} />
-        </Routes>
-      </>
-    );
-  };
-  
-  export default App;
+
+import { Route, Routes } from "react-router-dom";
+import { Welcome } from "./Welcome";
+import {Counter} from "./Counter"
+const App = () => {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="/:name" element={<Welcome />} />
+        <Route path="/counter" element={< Counter/>} />
+      </Routes>
+    </>
+  );
+};
